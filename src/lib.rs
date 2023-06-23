028aa54848f10188bd0b761b113933b312c023ef
Donald Dominic <gotodonald@gmail.com>
2023-06-23 07:31:56 +0100

    Add function get number from selected roman numerial

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! Roman crate provides the functionality to convert roman numerals into a u32 integer
 
-/// Roman numeral data type
+use std::collections::HashMap;
 
+/// Roman numeral data type
 pub struct Roman;
 
 impl Roman {
@@ -21,20 +22,42 @@ impl Roman {
     
 }
 
+/// ```get_number_from_roman``` function takes a ```&str``` input and returns
+/// 
+/// an ```Option<u32>``` value
+fn get_number_from_roman<'a>(v: &'a str) -> Option<u32> {
+    let mut rome: HashMap<String, u32> = HashMap::new();
+    rome.insert("I".to_string(), 1);
+    rome.insert("V".to_string(), 5);
+    rome.insert("X".to_string(), 10);
+    rome.insert("L".to_string(), 50);
+    rome.insert("C".to_string(), 100);
+    rome.insert("D".to_string(), 500);
+    rome.insert("M".to_string(), 1000);
+
+    let s = v.to_string().to_uppercase();
+
+    if let Some(x) = rome.get(&s) {
+        Some(x.to_owned())
+    } else {
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
-    use crate::{Roman};
+    use crate::{get_number_from_roman};
 
-    fn roman_input_and_output<'a>() -> Vec<(&'a str, u32)> {
-        vec![("i", 1), ("ii", 2), ("iii", 3), ("iv", 4), ("v", 5)]
+    fn roman_main_numbers<'a>() -> Vec<(&'a str, u32)> {
+        vec![("I", 1), ("V", 5), ("X", 10), ("l", 50), ("c", 100), ("d", 500), ("m", 1000)]
     }
 
     #[test]
-    fn test_roman() {
-        for v in roman_input_and_output() {
-            // let n = Rome::new(v.0);
-            // assert_eq!(v.1, n.to_int().unwrap());
+    fn test_get_number_from_rome() {
+        for v in roman_main_numbers() {
+            let n = get_number_from_roman(v.0).unwrap();
+            assert_eq!(n, v.1);
         }
     }
 }
